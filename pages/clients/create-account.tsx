5c8315e05e18768e7e1e527734eda4f006f8f111
Ricky Khao <khaoricky@gmail.com>
2023-07-25 15:35:06 +0200

    feat(login): ajout page création de compte

--- pages/clients/create-account.tsx
+++ pages/clients/create-account.tsx
@@ -1,45 +1,37 @@
 import { useState, FormEvent } from 'react';
 import { useRouter } from 'next/router';
-import Link from 'next/link';
 
-const LoginPage = () => {
+const CreateAccountPage = () => {
 	const [email, setEmail] = useState<string>('');
 	const [password, setPassword] = useState<string>('');
+	const [confirmPassword, setConfirmPassword] = useState<string>('');
 	const [errorMessage, setErrorMessage] = useState<string>('');
 
 	const router = useRouter();
-
 	//vérifier validité de l'email "user"@"mail"."fin"
 	const isValidEmail = (email: string): boolean => {
 		const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 		return emailRegex.test(email);
 	};
-
 	const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
 		setErrorMessage(''); // Réinitialise le message d'erreur
 
 		try {
-			// Vous pouvez envoyer les données (email et mot de passe) à votre backend pour la vérification.
-
-			// Exemple factice pour la démonstration
-			if (isValidEmail(email)) {
-			} else {
+			// Ajoutez ici la logique pour la création de compte
+			// Vous pouvez envoyer les données (email et mot de passe) à votre backend pour enregistrer le compte.
+			//Vérification Email et mot de passe avec mot de passe de confirmation
+			if (!isValidEmail(email) || password != confirmPassword) {
 				setErrorMessage('Email ou mot de passe incorrect');
 			}
 		} catch (error) {
-			setErrorMessage('Erreur lors de la connexion');
+			setErrorMessage('Erreur lors de la création du compte');
 		}
-		console.log('email : ' + email);
-		console.log('type email : ' + typeof email);
-		console.log('email is correct ? : ' + isValidEmail(email));
-		console.log('password :' + password);
-		console.log('type password : ' + typeof password);
 	};
 
 	return (
 		<div>
-			<h1>Page de connexion</h1>
+			<h1>Créer un compte</h1>
 			<form onSubmit={handleSubmit}>
 				<div>
 					<label htmlFor="email">Email</label>
@@ -59,15 +51,22 @@ const LoginPage = () => {
 						onChange={e => setPassword(e.target.value)}
 					/>
 				</div>
-				<button type="submit">Se connecter</button>
+				<div>
+					<label htmlFor="confirmPassword">
+						Confirmer le mot de passe
+					</label>
+					<input
+						id="confirmPassword"
+						type="password"
+						value={confirmPassword}
+						onChange={e => setConfirmPassword(e.target.value)}
+					/>
+				</div>
+				<button type="submit">Créer le compte</button>
 			</form>
 			{errorMessage && <p>{errorMessage}</p>}
-			<p>Pas encore de compte ?</p>
-			<Link href="./create-account">
-				<p>Créer un compte</p>
-			</Link>
 		</div>
 	);
 };
 
-export default LoginPage;
+export default CreateAccountPage;
