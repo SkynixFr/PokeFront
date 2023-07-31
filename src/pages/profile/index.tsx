f18decb6518506270eff1541e733fc75c80abb46
Suranewt <saranyudaorueang@gmail.com>
2023-07-31 10:49:30 +0200

    Refactor(Profile) : suppresion niveau dresseur + ajout css liste pokemons

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -18,7 +18,7 @@ const Profil: React.FC = () => {
 
 	useEffect(() => {
 		const jwtToken =
-			'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0YzM2OGFhNzAyZTUyMjE4NWQ0OGUxZSIsInVzZXJuYW1lIjoiTHVmZnlzb25pYyIsImVtYWlsIjoicmlja3lAZ21haWwuY29tIiwicGFzc3dvcmQiOiIkMmIkMTAkeUFhQndmYzVPYVVrOU84TG1CTXprZWlubW14OC9aTmpwSW1UcnkvbEI3SVVVLnBNMloucksiLCJwb2tlZGV4IjpbInBpa2FjaHUiLCJyaW9sdSJdLCJjcmVhdGVkQXQiOiIyMDIzLTA3LTI4VDA3OjA1OjEzLjk1N1oiLCJ1cGRhdGVBdCI6IjIwMjMtMDctMjhUMTM6MTU6MzYuOTU2WiIsImlhdCI6MTY5MDU1NTY5MiwiZXhwIjoxNjkwNTU2MjkyfQ.j7j8P9l6PmCSNLt8rBAN7LkmLWAE2XE24GT3Bt3cYbs';
+			'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0YzM2OGFhNzAyZTUyMjE4NWQ0OGUyMCIsInVzZXJuYW1lIjoiTmV3dCIsImVtYWlsIjoic2FyYW55dUBnbWFpbC5jb20iLCJwYXNzd29yZCI6IiQyYiQxMCRNYnl3UEZiWEZMLlowWk12SUZ0bGdPcU9vaVlZUVZicDF4aDBFdEh0cW5hTy8vaXp1T0EvTyIsInBva2VkZXgiOlsicGlrYWNodSIsInJpb2x1Il0sImNyZWF0ZWRBdCI6IjIwMjMtMDctMjhUMDc6MDU6MTQuNDA0WiIsInVwZGF0ZUF0IjoiMjAyMy0wNy0yOFQxMzoxMjo1OS4yNjNaIiwiaWF0IjoxNjkwNzQ0NzE3LCJleHAiOjE2OTA3NDUzMTd9.VsGM-0vbTpPvTNn3ReihY9ycsKdcNPwPnKUOzfSdXus';
 		localStorage.setItem('jwtToken', jwtToken);
 
 		const config = {
@@ -49,7 +49,7 @@ const Profil: React.FC = () => {
 
 	const trainerName = userData?.username;
 	const trainerEmail = userData?.email;
-	const trainerPokedex = userData?.pokedex;
+	const trainerPokedex = userData?.pokedex || [];
 	const trainerLevel = 50;
 	const progression = 75;
 
@@ -64,9 +64,9 @@ const Profil: React.FC = () => {
 			<div className={styles.profileContainer}>
 				<h1 className={styles.trainerName}>Pseudo : {trainerName}</h1>
 				<h1 className={styles.trainerName}>Email : {trainerEmail}</h1>
-				<p className={styles.trainerLevel}>
+				{/* <p className={styles.trainerLevel}>
 					Niveau de dresseur : {trainerLevel}
-				</p>
+				</p> */}
 
 				<div className={styles.progressBar}>
 					<div
@@ -82,12 +82,12 @@ const Profil: React.FC = () => {
 				{/* Le reste du contenu du profil Pokémon */}
 			</div>
 
-			<div className="pokemon-list">
-				{/* {trainerPokedex.map(item => (
-					<div key={item} className="pokemon-card">
-						<p className="pokemon-name">{item}</p>
+			<div className={styles.pokemonList}>
+				{trainerPokedex.map(item => (
+					<div key={item} className={styles.pokemonCard}>
+						<p className={styles.pokemonName}>{item}</p>
 					</div>
-				))} */}
+				))}
 			</div>
 		</>
 	);
